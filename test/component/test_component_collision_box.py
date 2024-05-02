2ba22531f370dd16e8c5467f9ddaf775936c1209
TheBlackMezek <31017211+TheBlackMezek@users.noreply.github.com>
2024-05-01 17:05:01 -0700

    Correct component_collision_box() unit test names

--- test/component/test_component_collision_box.py
+++ test/component/test_component_collision_box.py
@@ -1,13 +1,13 @@
 from brockassemble.component import component_collision_box
 
 
-def test_arg_range():
+def test_arg_width():
     test_value = 1.0
     comp = component_collision_box(test_value, 2.0)
     assert comp.json_obj['width'] == test_value
 
 
-def test_arg_dmg():
+def test_arg_height():
     test_value = 2.0
     comp = component_collision_box(1.0, test_value)
     assert comp.json_obj['height'] == test_value
